b4d30dfc64a71abc1471111a07f840158f792d50
Sai Num Town <sainumtown@gmail.com>
2017-06-08 14:15:12 +0630

    first commit

---
+++ PADC2_REGISTER_LOGIN/app/src/main/java/delivery/sainumtown/xyz/barlarlardelivery/MainActivity.java
@@ -0,0 +1,13 @@
+package delivery.sainumtown.xyz.barlarlardelivery;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
